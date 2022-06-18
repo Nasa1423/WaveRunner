42c84c61c83ed605401aa3ee368f516e7c3ebc8b
aloncess <90398251+aloncess@users.noreply.github.com>
2022-06-19 01:46:51 +0300

    FUUUCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCk

--- Source/WaveRunner/SpikeTrap.cpp
+++ Source/WaveRunner/SpikeTrap.cpp
@@ -3,10 +3,15 @@
 
 #include "SpikeTrap.h"
 #include "Runtime/Engine/Public/EngineGlobals.h"
+//#include ""
 
 // Sets default values
 ASpikeTrap::ASpikeTrap()
-{
+{	
+
+	requestedHeight = 0;
+	speed = 0;
+	bIsMoving = false;
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -18,6 +23,8 @@ ASpikeTrap::ASpikeTrap()
 	SplineComponent->Duration = TotalPathTimeController;
 	SpikeMesh->SetupAttachment(SplineComponent);
 
+	CurrentLocation = SpikeMesh->GetComponentLocation();
+
 	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger Box"));
 	TriggerBox->AttachToComponent(RootComponent, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true));
 	TriggerBox->InitBoxExtent(FVector(100.0f, 100.0f, 100.0f));
@@ -25,19 +32,34 @@ ASpikeTrap::ASpikeTrap()
 	TriggerBox->SetCollisionProfileName(TEXT("Trigger"));
 
 	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &ASpikeTrap::OnOverlapBegin);
-
+	
 	StartTime = 0;
 
+
+	//ProgressFunction.BindUFunction(this, TEXT("ProcessMovementTimeline"));
+	//MovementTimeline.AddInterpFloat(MovementCurve, ProgressFunction);
+	//FOnTimelineEvent OnTimelineFinishedFunction;
+	//OnTimelineFinishedFunction.BindUFunction(this, TEXT("OnEndMovementTimeline"));
+	//MovementTimeline.SetTimelineFinishedFunc(OnTimelineFinishedFunction);
+	//MovementTimeline.SetTimelineLengthMode(TL_LastKeyFrame);
+	//if (bAutoActivate)
+	//{
+	//	MovementTimeline.PlayFromStart();
+	//}
 
 }
 void ASpikeTrap::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
 	GEngine->AddOnScreenDebugMessage(-1, 12.f, FColor::White, TEXT("Simple message"));
+	//moveUp(500);
 }
 
 // Called when the game starts or when spawned
 void ASpikeTrap::BeginPlay()
 {
 	Super::BeginPlay();
+
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &ASpikeTrap::TriggerBeginOverlap);
+	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &ASpikeTrap::TriggerEndOverlap);
 
 	StartTime = GetWorld()->GetTimeSeconds();
 	
@@ -47,6 +69,18 @@ void ASpikeTrap::BeginPlay()
 void ASpikeTrap::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	if (bIsMoving) {
+		if (requestedHeight - CurrentLocation.Z >= speed) {
+			CurrentLocation.Z += speed;
+		}
+		else {
+			CurrentLocation.Z = requestedHeight;
+		}
+		SpikeMesh->SetWorldLocation(CurrentLocation);
+		if (CurrentLocation.Z == requestedHeight) {
+			bIsMoving = false;
+		}
+	}
 
 	float CurrentSplineTime = (GetWorld()->GetTimeSeconds() - StartTime) / TotalPathTimeController;
 
@@ -75,3 +109,26 @@ void ASpikeTrap::Tick(float DeltaTime)
 	}
 }
 
+void ASpikeTrap::ProcessMovementTimeline() {
+
+}
+
+void ASpikeTrap::OnEndMovementTimeline() {
+
+}
+
+void ASpikeTrap::TriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+	FOnTimelineFloat ProgressFunction;
+	ProgressFunction.BindUFunction(this, TEXT("ProcessMovementTimeline"));
+	MovementTimeline.AddInterpFloat(MovementCurve, ProgressFunction);
+}
+
+void ASpikeTrap::TriggerEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {
+
+}
+
+//void ASpikeTrap::moveUp(float points) {
+	//bIsMoving = true;
+	//requestedHeight = CurrentLocation.Z + points;
+	//speed = 150.0f;
+//}
--- Source/WaveRunner/SpikeTrap.h
+++ Source/WaveRunner/SpikeTrap.h
@@ -6,41 +6,80 @@
 #include "GameFramework/Actor.h"
 #include "Components/SplineComponent.h"
 #include "Components/BoxComponent.h"
+#include "Components/TimelineComponent.h"
 #include "SpikeTrap.generated.h"
 
 UCLASS()
 class WAVERUNNER_API ASpikeTrap : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	ASpikeTrap();
-	
+
+private:
+	FVector CurrentLocation;
+	float speed;
+	bool bIsMoving;
+	float requestedHeight;
+	UPROPERTY(EditAnywhere, Category = "Custom")
+		double LinearSpeed;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(EditAnywhere, Category = "SplineController")
-	float TotalPathTimeController;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UTimelineComponent* ExampleTimelineComp;
 
 	UPROPERTY(EditAnywhere, Category = "SplineController")
-	bool bSplineInLoop;
+		float TotalPathTimeController;
+
+	UPROPERTY(EditAnywhere, Category = "SplineController")
+		bool bSplineInLoop;
 
 	bool bCanMoveActor;
 
 	float StartTime;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+		void ProcessMovementTimeline();
+
+	UFUNCTION()
+		void OnEndMovementTimeline(); 
+
+	UFUNCTION()
+		void TriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void TriggerEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	//UFUNCTION()
+		//void moveUp(float points);
+
+
+
+public:
 	UPROPERTY(VisibleAnywhere)
 		UStaticMeshComponent* SpikeMesh;
 	UPROPERTY(VisibleAnywhere)
 		USplineComponent* SplineComponent;
 	UPROPERTY(VisibleAnywhere)
 		UBoxComponent* TriggerBox;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spline", meta = (AllowPrivateAccess = "true"))
+		UCurveFloat* MovementCurve;
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SplineEdit", meta = (AllowPrivateAccess = "true"))
+		//int data;
+	
 
 	UFUNCTION()
 		void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	FTimeline MovementTimeline;
+	//FOnTimelineFloat ProgressFunction;
 };
