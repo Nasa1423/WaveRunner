b5ae8e75b80da758cfe201fefa4b288983896e0e
aloncess <90398251+aloncess@users.noreply.github.com>
2022-06-27 00:16:02 +0300

    Removed old debug

--- Source/WaveRunner/SpikeTrap.cpp
+++ Source/WaveRunner/SpikeTrap.cpp
@@ -32,7 +32,7 @@ ASpikeTrap::ASpikeTrap()
 
 }
 void ASpikeTrap::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
-	GEngine->AddOnScreenDebugMessage(-1, 12.f, FColor::White, TEXT("Simple message"));
+	//GEngine->AddOnScreenDebugMessage(-1, 12.f, FColor::White, TEXT("Simple message"));
 
 }
 
